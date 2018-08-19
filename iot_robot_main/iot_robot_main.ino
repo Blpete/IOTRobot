ee60dc573d2e22d00f3bfc44fcd30c9a0fce9eac
Barry L Peterson <blpete@gmail.com>
2018-08-18 22:13:47 -0400

    formatted, added command parser

--- iot_robot_main/iot_robot_main.ino
+++ iot_robot_main/iot_robot_main.ino
@@ -7,7 +7,6 @@
 // Wifi
 #include <ESP8266WiFi.h>
 
-
 // Managing common includes
 // I2C
 #include <Wire.h>
@@ -26,21 +25,19 @@
 //  MQTT Interface
 #include <PubSubClient.h>
 
-
-#include <LiquidCrystal_I2C.h> //This library you can add via Include Library > Manage Library > 
-
+#include <LiquidCrystal_I2C.h> //This library you can add via Include Library > Manage Library >
 
 //
 // Configuration Data
 //
 
 // MicroController ID
-#define DEVICE_ID ESP8266-1
+const char *DEVICE_ID = "ESP8266-1";
 
 // Wifi Settings
-char ssid[] = "iotlink";     //  your network SSID (name)
-char pass[] = "2603380139!"; //"peterson1";  // your network password
-int wifiStatus = WL_IDLE_STATUS;     // the Wifi radio's status
+char ssid[] = "iotlink";         //  your network SSID (name)
+char pass[] = "2603380139!";     //"peterson1";  // your network password
+int wifiStatus = WL_IDLE_STATUS; // the Wifi radio's status
 WiFiClient wifiClient;
 
 // Real Time Clock
@@ -49,111 +46,115 @@ WiFiClient wifiClient;
 // MQTT configurationPubSubClient client(espClient);
 // dependency on Wifi
 PubSubClient mqttClient(wifiClient);
-const char* mqttServer = "10.0.0.28";
+const char *mqttServer = "10.0.0.28";
 const int mqttPort = 1883;
-const char* mqttUser = "blpete";
-const char* mqttPassword = "Peterson2016!";
-const char* nodeName ="ESP8266-1";
-boolean mqttInitialized= false;
+const char *mqttUser = "blpete";
+const char *mqttPassword = "Peterson2016!";
+const char *nodeName = "ESP8266-1";
+boolean mqttInitialized = false;
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-// One time setup 
+// One time setup
 void setup()
 {
-    //Setup Serial Communications
-    Serial.begin(115200);
-    logMessage("Serial Interface started at 115200 baud");
+  //Setup Serial Communications
+  Serial.begin(115200);
+  logMessage("Serial Interface started at 115200 baud");
 
-    // Setup I2C communications
-    Wire.begin();
-    logMessage("I2C communicaton started");
+  // Setup I2C communications
+  Wire.begin();
+  logMessage("I2C communicaton started");
 
-    // Configure Wifi
-    configureWifi();
+  // Configure Wifi
+  configureWifi();
 
-    // Real Time Clock
-    startupRTC();
+  // Real Time Clock
+  startupRTC();
 
-    // startup OTA
-    startupOTA();
+  // startup OTA
+  startupOTA();
 
-    // startup MQTT
-    startupMQTT();
+  // startup MQTT
+  startupMQTT();
 
-    // Configure Device
-    logMessage("Device Initialized:"+ DEVICE_ID);
-lcd.begin(16,2);
-    lcd.init();   // initializing the LCD
-  lcd.backlight(); // Enable or Turn On the backlight 
+  // Configure Device
+  logMessage("Device Initialized:");
+  logMessage(DEVICE_ID);
+  lcd.begin(16, 2);
+  lcd.init();      // initializing the LCD
+  lcd.backlight(); // Enable or Turn On the backlight
   lcd.setCursor(0, 1);
   lcd.print("14CORE | 16x2 LCD TEST"); // Start Print text to Line 1
-  lcd.setCursor(0, 2);      
+  lcd.setCursor(0, 2);
   lcd.print("-----------------------"); // Start Print Test to Line 2
+ // lcdInitialized = true;
 }
- 
-// Main Looop 
+
+// Main Looop
 void loop()
 {
-    scanI2CBus();
+  //scanI2CBus();
 
- //   logRealTimeClock();
+  //   logRealTimeClock();
 
-    // OTA update loop
-    ArduinoOTA.handle();
+  // OTA update loop
+  ArduinoOTA.handle();
 
-    // MQTT loop
-    mqttClient.loop();
+  // MQTT loop
+  mqttClient.loop();
 
-    delay(2000);
+  delay(2000);
 }
-
 
 // Common Utilities
-void logMessage(char* msg){
-    Serial.println(msg);
-    if (mqttInitialized) {
-      mqttClient.publish("esp/log", msg);
-    }
+void logMessage(const char *msg)
+{
+  Serial.println(msg);
+  if (mqttInitialized)
+  {
+    mqttClient.publish("esp/log", msg);
+  }
 }
 
-void logPrint(char* msg) {
-    Serial.print(msg);
+void logPrint(char *msg)
+{
+  Serial.print(msg);
 }
-
 
 // I2C common Utilities
-void scanI2CBus() {
+void scanI2CBus()
+{
   byte error, address;
   int nDevices;
- 
+
   logMessage("Scanning I2C Bus...");
- 
+
   nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
+  for (address = 1; address < 127; address++)
   {
- 
+
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
- 
+
     if (error == 0)
     {
       logMessage("I2C device found at address 0x");
-      if (address<16) 
+      if (address < 16)
         Serial.print("0");
-        Serial.println(address,HEX);
-    //todo  logPrint(address,HEX);
-     logMessage("  !");
- 
+      Serial.println(address, HEX);
+      //todo  logPrint(address,HEX);
+      logMessage("  !");
+
       nDevices++;
     }
-    else if (error==4) 
+    else if (error == 4)
     {
       logMessage("I2C Unknown error at address 0x");
-      if (address<16) 
+      if (address < 16)
         logPrint("0");
-        Serial.print(address,HEX);
-    }    
+      Serial.print(address, HEX);
+    }
   }
   if (nDevices == 0)
     logMessage("No I2C devices found\n");
@@ -161,55 +162,56 @@ void scanI2CBus() {
     logMessage("I2C scan done\n");
 }
 
-
 //  Configure Wifi subsystem
 void configureWifi()
 {
-    WiFi.mode(WIFI_STA); // set to station mode
+  WiFi.mode(WIFI_STA); // set to station mode
 
-    scanWifiNetworks();
-    
-    while (wifiStatus != WL_CONNECTED) {
-      logMessage("Attempting to connect to WPA SSID: ");
-      logMessage(ssid);
-      // Connect to WPA/WPA2 network:
-      wifiStatus = WiFi.begin(ssid, pass);
+  scanWifiNetworks();
 
-      // wait 10 seconds for connection:
-      delay(5000);
-    }
-    printWifiData();
+  while (wifiStatus != WL_CONNECTED)
+  {
+    logMessage("Attempting to connect to WPA SSID: ");
+    logMessage(ssid);
+    // Connect to WPA/WPA2 network:
+    wifiStatus = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(5000);
+  }
+  printWifiData();
 }
 
 // Log WIFI configuration Data
-void printWifiData() {
+void printWifiData()
+{
   // print the SSID of the network you're attached to:
   logMessage("SSID: ");
- // logMessage(WiFi.SSID());
- 
+  // logMessage(WiFi.SSID());
 
-    // print your WiFi shield's IP address:
+  // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   logPrint("IP Address: ");
   Serial.println(ip);
- // logMessage(ip);
-
+  lcd.setCursor(0,1);
+  lcd.print(ip);
+  // logMessage(ip);
 
   // print your MAC address:
   byte mac[6];
   WiFi.macAddress(mac);
   logPrint("MAC address: ");
-//  logPrint(mac[5], HEX);
-//  logPrint(":");
-//  logPrint(mac[4], HEX);
-//  logPrint(":");
-//  logPrint(mac[3], HEX);
-//  logPrint(":");
-//  logPrint(mac[2], HEX);
-//  logPrint(":");
-//  logPrint(mac[1], HEX);
-//  logPrint(":");
-//  logPrint(mac[0], HEX);
+  //  logPrint(mac[5], HEX);
+  //  logPrint(":");
+  //  logPrint(mac[4], HEX);
+  //  logPrint(":");
+  //  logPrint(mac[3], HEX);
+  //  logPrint(":");
+  //  logPrint(mac[2], HEX);
+  //  logPrint(":");
+  //  logPrint(mac[1], HEX);
+  //  logPrint(":");
+  //  logPrint(mac[0], HEX);
 
   // Wifi signal strength
   long rssi = WiFi.RSSI();
@@ -217,16 +219,21 @@ void printWifiData() {
   Serial.println(rssi);
 }
 
-void scanWifiNetworks() {
- // WiFi.scanNetworks will return the number of networks found
+void scanWifiNetworks()
+{
+  // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
-  if (n == 0) {
+  if (n == 0)
+  {
     Serial.println("no networks found");
-  } else {
+  }
+  else
+  {
     Serial.print(n);
     Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
+    {
       // Print SSID and RSSI for each network found
       Serial.print(i + 1);
       Serial.print(": ");
@@ -241,50 +248,50 @@ void scanWifiNetworks() {
   Serial.println("");
 }
 
-
-void startupRTC() {
+void startupRTC()
+{
   setRealTimeClock();
 }
 
-void setRealTimeClock(){
-   logMessage("RTC is being set!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    //rtc.adjust(DateTime(__DATE__, __TIME__));
-   // rtc.SetTime(00,40,13,1,06,03,17);
+void setRealTimeClock()
+{
+  logMessage("RTC is being set!");
+  // following line sets the RTC to the date & time this sketch was compiled
+  //rtc.adjust(DateTime(__DATE__, __TIME__));
+  // rtc.SetTime(00,40,13,1,06,03,17);
 }
 
-void logRealTimeClock () {
-   // rtc.DisplayTime();
-    long now = 0;//rtc.now();
-    logMessage("RTC value:");
-    Serial.println(now);
-//    Serial.print(now.year(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.month(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.day(), DEC);
-//    Serial.print(' ');
-//    Serial.print(now.hour(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.minute(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.second(), DEC);
-//    Serial.println();
-//    
-//    Serial.print(" since midnight 1/1/1970 = ");
-//    Serial.print(now.unixtime());
-//    Serial.print("s = ");
-//    Serial.print(now.unixtime() / 86400L);
-//    Serial.println("d");
-//    
-//      Serial.println();
-
+void logRealTimeClock()
+{
+  // rtc.DisplayTime();
+  long now = 0; //rtc.now();
+  logMessage("RTC value:");
+  Serial.println(now);
+  //    Serial.print(now.year(), DEC);
+  //    Serial.print('/');
+  //    Serial.print(now.month(), DEC);
+  //    Serial.print('/');
+  //    Serial.print(now.day(), DEC);
+  //    Serial.print(' ');
+  //    Serial.print(now.hour(), DEC);
+  //    Serial.print(':');
+  //    Serial.print(now.minute(), DEC);
+  //    Serial.print(':');
+  //    Serial.print(now.second(), DEC);
+  //    Serial.println();
+  //
+  //    Serial.print(" since midnight 1/1/1970 = ");
+  //    Serial.print(now.unixtime());
+  //    Serial.print("s = ");
+  //    Serial.print(now.unixtime() / 86400L);
+  //    Serial.println("d");
+  //
+  //      Serial.println();
 }
 
-
-
-void startupOTA() {
- // Port defaults to 8266
+void startupOTA()
+{
+  // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
 
   // Hostname defaults to esp8266-[ChipID]
@@ -315,96 +322,112 @@ void startupOTA() {
   });
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
   });
   ArduinoOTA.begin();
-  }
+}
 
-
-void startupMQTT() {
-    // setup mqtt
+void startupMQTT()
+{
+  // setup mqtt
   mqttClient.setServer(mqttServer, mqttPort);
- 
-  while (!mqttClient.connected()) {
-      Serial.println("Connecting to MQTT...");
-   
-      if (mqttClient.connect(nodeName,mqttUser, mqttPassword )) {
-        logMessage("MQTT connected");
-      } else {
-        logMessage("MQTT failed with state:");
-        Serial.println(mqttClient.state());
-        delay(2000);
-   
-      }
+
+  while (!mqttClient.connected())
+  {
+    Serial.println("Connecting to MQTT...");
+
+    if (mqttClient.connect(nodeName, mqttUser, mqttPassword))
+    {
+      logMessage("MQTT connected");
+    }
+    else
+    {
+      logMessage("MQTT failed with state:");
+      Serial.println(mqttClient.state());
+      delay(2000);
+    }
   }
   mqttInitialized = true;
   mqttClient.publish("esp/test", "Hello from ESP");
-  
+
   mqttClient.subscribe("esp/msg");
   mqttClient.setCallback(mqttCallback);
 }
 
-
-
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
-//  Serial.println();
-//      char cstring[length];
-//      for (int i=0; i<length; i++){
-//        cstring[i]=(char) payload[i];
-//      }
-//  cstring[length] = '\0';    // Adds a terminate terminate to end of string based on length of current payload
-// const char* p_payload = cstring;  
-//  Serial.println(p_payload);   
-//    //String s = String((char*)p_payload);
+  //  Serial.println();
+  //      char cstring[length];
+  //      for (int i=0; i<length; i++){
+  //        cstring[i]=(char) payload[i];
+  //      }
+  //  cstring[length] = '\0';    // Adds a terminate terminate to end of string based on length of current payload
+  // const char* p_payload = cstring;
+  //  Serial.println(p_payload);
+  //    //String s = String((char*)p_payload);
 
   payload[length] = '\0'; // Null terminator used to terminate the char array
-  String message = (char*)payload;
+  String message = (char *)payload;
   Serial.println(message);
-  
-    //logMessage(p_payload);
-    //int strcomparison = strcmp(p_payload, "on");
-    if (message=="off") {
-       Serial.println("off");
-        lcd.noBacklight();
-  
-    } //end If 
-    else {
-             Serial.println("on");
-        lcd.backlight();
-    }
-    
-                             
-    lcd.clear();
 
-    lcd.setCursor(0,0);
-   lcd.print(message);
-//    Serial.print("] ");
-//    for (int i = 0; i < length; i++) {
-//      Serial.print((char)payload[i]);
-//    }
-//    Serial.println();
-//    const char * stupid ="stupid";
-//    mqttClient.publish("esp/test", stupid);
-//    Serial.print("Msg:");
-//    Serial.print(stupid);
+  executeCommand(message);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+ // lcd.autoscroll();
+  lcd.print(message);
+ 
+  //    Serial.print("] ");
+  //    for (int i = 0; i < length; i++) {
+  //      Serial.print((char)payload[i]);
+  //    }
+  //    Serial.println();
+  //    const char * stupid ="stupid";
+  //    mqttClient.publish("esp/test", stupid);
+  //    Serial.print("Msg:");
+  //    Serial.print(stupid);
 
   // // Switch on the LED if an 1 was received as first character
-  // if ((char)payload[0] == '1') {
-  //   digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  //   // but actually the LED is on; this is because
-  //   // it is acive low on the ESP-01)
-  // } else {
-  //   digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  // }
+}
 
+void executeCommand(String message)
+{
+  if (message == "lcd-off")
+  {
+    Serial.println("off");
+    lcd.noBacklight();
+  }
+  else if (message == "lcd-on")
+  {
+    Serial.println("on");
+    lcd.backlight();
+  }
+  else if (message == "pin1-on")
+  {
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+  }
+  else if (message == "pin1-off")
+  {
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+  else if (message == "scan")
+  {
+    scanI2CBus();
+  }
 }
